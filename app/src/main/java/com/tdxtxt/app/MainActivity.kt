5c76f7c4c94cecb6b2be9f2a3dab55bef4738bf5
tangdexiang <tangdexiang@ccqtgb.com>
2023-03-10 10:57:18 +0800

    demo

--- app/src/main/java/com/tdxtxt/app/MainActivity.kt
+++ app/src/main/java/com/tdxtxt/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tdxtxt.app
 
+import android.content.Intent
 import com.tdxtxt.baselib.ui.CommToolBarActivity
 import com.tdxtxt.routercore.moduleservice.MduserRouterService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +13,8 @@ class MainActivity : CommToolBarActivity() {
         setTitleBar("我是大人")
 
         btn_next_1.setOnClickListener {
-            MduserRouterService.login(this@MainActivity, "122", "33dd")
+            startActivity(Intent(fragmentActivity, VideoActivity::class.java))
+//            MduserRouterService.login(this@MainActivity, "122", "33dd")
         }
     }
 }
---
+++ app/src/main/java/com/tdxtxt/app/VideoActivity.kt
@@ -0,0 +1,26 @@
+package com.tdxtxt.app
+
+import com.tdxtxt.baselib.ui.CommToolBarActivity
+import com.tdxtxt.video.VideoPlayerManager
+import kotlinx.android.synthetic.main.activity_video_test.*
+
+/**
+ * <pre>
+ *     author : ton
+ *     time   : 2023/2/15
+ *     desc   :
+ * </pre>
+ */
+class VideoActivity : CommToolBarActivity(){
+    override fun getLayoutResId() = R.layout.activity_video_test
+
+    override fun initStatusBar() {
+//        StatusBarHelper.setDarkMode(fragmentActivity)
+    }
+
+    override fun initUi() {
+        videoPlayer.setVideoPlayer(VideoPlayerManager.newInstance().getVideoPlayer())
+        videoPlayer.setDataSource("https://mediaapi.juexiaotime.com/1111112023年法考资料/肖沛权/内部课/40diwuzhang.mp4")
+        videoPlayer.start()
+    }
+}
